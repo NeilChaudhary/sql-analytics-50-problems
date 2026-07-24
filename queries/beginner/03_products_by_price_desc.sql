d2e478154acbdf852cf650ac7326c7a9773ae83f
Neil Chaudhary <190195530+NeilChaudhary@users.noreply.github.com>
2026-07-24 13:08:45 +0530

    Update 03_products_by_price_desc.sql

--- queries/beginner/03_products_by_price_desc.sql
+++ queries/beginner/03_products_by_price_desc.sql
@@ -1,1 +1,13 @@
+/*
+Problem 03: Products by Price (Descending)
 
+Objective:
+Retrieve the ProductID, ProductName, and UnitPrice of all products, sorted by UnitPrice in descending order.
+
+Concepts Used:
+- SELECT
+- ORDER BY
+*/
+SELECT ProductID, ProductName, UnitPrice
+FROM Products
+ORDER BY UnitPrice DESC;
