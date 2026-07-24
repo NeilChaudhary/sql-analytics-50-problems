2a82bca3dff00aa31b0a88270cf5f324f00e6258
Neil Chaudhary <190195530+NeilChaudhary@users.noreply.github.com>
2026-07-24 13:05:59 +0530

    Create SQL query for customers from Germany
    Added SQL query to retrieve customers from Germany.

--- queries/beginner/02_customers_from_germany.sql
+++ queries/beginner/02_customers_from_germany.sql
@@ -1,1 +1,14 @@
+/*
+Problem 02: Customers from Germany
 
+Objective:
+Retrieve the CustomerID, CompanyName, and Country of all customers from Germany.
+
+Concepts Used:
+- SELECT
+- WHERE
+*/
+
+SELECT CustomerID, CompanyName, Country
+FROM Customers
+WHERE Country = 'Germany';
