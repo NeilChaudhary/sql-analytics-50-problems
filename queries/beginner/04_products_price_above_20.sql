0ddda7d16da1693514ca64a99d080b31430bbbd1
Neil Chaudhary <190195530+NeilChaudhary@users.noreply.github.com>
2026-07-24 13:13:23 +0530

    Solve Problem 04 - Products above $20 sorted by price

--- queries/beginner/04_products_price_above_20.sql
+++ queries/beginner/04_products_price_above_20.sql
@@ -1,1 +1,15 @@
+/*
+Problem 04: Products with Unit Price Above 20
 
+Objective:
+Retrieve the ProductID, ProductName, and UnitPrice of all products with a UnitPrice greater than 20. Display the results in ascending order of UnitPrice.
+
+Concepts Used:
+- SELECT
+- WHERE
+- ORDER BY
+*/
+SELECT ProductID, ProductName, UnitPrice
+FROM Products
+WHERE UnitPrice > 20
+ORDER BY UnitPrice ASC;
