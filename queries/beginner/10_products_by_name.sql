696a8ad6c4021d448afd72385a0a5fe93a2f9147
Neil Chaudhary <190195530+NeilChaudhary@users.noreply.github.com>
2026-08-06 04:44:32 +0530

    Solve Problem 10 - Sort products alphabetically

---
+++ queries/beginner/10_products_by_name.sql
@@ -0,0 +1,10 @@
+/*
+Problem 10: Sort Products Alphabetically
+
+Objective:
+Retrieve the ProductID and ProductName of all products and display them in alphabetical order by ProductName.
+
+Concepts Used:
+- SELECT
+- ORDER BY
+*/
